044a9a8c3b9bb6a256cfa00d727e716027ab9e98
kbpak <kbpak98@omsecurity.kr>
2026-07-21 09:34:10 +0900

    default log path 롤백

--- util/common.go
+++ util/common.go
@@ -91,6 +91,5 @@ func resolveLogPath() string {
 	if p := configs.SC.Setting.LogPath; p != "" {
 		return p
 	}
-	//return fmt.Sprintf("%s/%s", configs.SC.Setting.RootPath, "log", "monitoring")
-	return path.Join(configs.SC.Setting.RootPath, "log", "monitoring")
+	return path.Join(configs.SC.Setting.RootPath, "log")
 }
